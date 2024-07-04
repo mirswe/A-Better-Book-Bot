fc1d5f56952f7a4d38f795e51cd15a00b76aa1cd
mirswe <ramirdixon123@gmail.com>
2024-07-04 01:25:55 -0400

    improved optimization for output fixed cpu 100% bug added new notes for upcoming changes

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ text = get_book_text(path_to_file)
 chunks = chunk_text(text)
 
 print("--- Begin report of texts ---\n")
-summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
+summarizer = pipeline("summarization", model="facebook/bart-large-cnn", min_length = 5, max_length = 20)
 
 summaries = [summarizer(chunk)[0]['summary_text'] for chunk in chunks]
 model_text = " ".join(summaries)
@@ -24,6 +24,7 @@ print("\n--- End report ---")
 
 '''
 Next Features to implement 
+    - grammer checker ; add openai 3.5 model and assign the model_text to a variable to have open ai quick check grammar and spit it back out under "Begin Reports of texts"
     - text selector ; choose what texts from the texts/ to choose from
     - clean up output ; make the printed output less janky
     - slow down output ; provide sleep() functions to slow down the output
